7bf19acc59cbb700d804abb578e5540ac6e9b903
Christoph Held <christoph.held@allianz.de>
2023-02-28 09:15:14 +0000

    add devcontainer and pipeline
    Signed-off-by: Christoph Held <christoph.held@allianz.de>

---
+++ .devcontainer/features/peribolos/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git clone https://github.com/kubernetes/test-infra.git
+cd test-infra
+git reset --hard f4414f57345035fbf7c93a734c9e9980eaab9c02
+
+go build -o peribolos ./prow/cmd/peribolos/main.go
+cp ./peribolos /usr/local/bin
+
+go build -o proxy ./ghproxy/ghproxy.go
+cp ./proxy /usr/local/bin
