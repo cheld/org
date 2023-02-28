cf19394039b5836809a3a9881eaf1f064f0fe9f9
Christoph Held <christoph.held@allianz.de>
2023-02-28 16:33:42 +0000

    test update in github action
    Signed-off-by: Christoph Held <christoph.held@allianz.de>

--- scripts/run-peribolos.sh
+++ scripts/run-peribolos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+cd "$(dirname "$0")"/..
 
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "Variable GITHUB_TOKEN not set"
     exit 1
 fi
-
-ORG="test-user-org"
+ORG=${1:-"test-user-org"} 
 
 ramtmp="$(mktemp -p /dev/shm/)"
 echo $GITHUB_TOKEN > $ramtmp
 
-peribolos --dump $ORG --github-token-path $ramtmp | tee ~/current.yaml
+peribolos --github-token-path $ramtmp --config-path org.yaml -fix-org --confirm
